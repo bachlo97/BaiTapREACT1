f879e99bb208bfc5f0afc37c5471995b7f2e89cd
bachlo97 <cuongpt8997@gmail.com>
2024-01-11 21:04:49 +0700

    loading 70%

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import Header from './modules/BaiTapThucHanhLayout/header'
 import Body from './modules/BaiTapThucHanhLayout/body'
 import Footer from './modules/BaiTapThucHanhLayout/footer'
+import BaiTapThucHanhLayout from './modules/BaiTapThucHanhLayout/BaiTapThucHanhLayout'
 
 function App() {
   return (
     <>
-      <Header/>
-      <Body/>
-      <Footer/>
+      <BaiTapThucHanhLayout/>
     </>
   )
 }
--- src/modules/BaiTapThucHanhLayout/body.jsx
+++ src/modules/BaiTapThucHanhLayout/body.jsx
@@ -8,24 +8,21 @@ export default class Body extends Component {
   addItem() {
     const count = 6;
     const items = [];
-
-    for (let i in count) {
+  
+    for (let i = 0; i < count; i++) {
       items.push(
-        <div className='col-lg-6 col-xxl-4 mb-5'>
+        <div key={i} className='col-lg-6 col-xxl-4 mb-5'>
           <Item />
         </div>
       );
     }
-
-    console.log('1235134',items)
     return items;
   }
-
+  
   render() {
     return (
       <>
         <Banner />
-        <Item/>
         <section className='pt-4'>
           <div className='class="container px-lg-5'>
             <div className='row gx-lg-5'>
